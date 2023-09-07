d58d75b53151256b5caab630731e758d1315b7d0
Eugene Kushnir <program.eugene@gmail.com>
2023-09-07 14:41:21 +0300

    better files structure. Starter template for dev

--- client/src/App.js
+++
@@ -1,7 +0,0 @@
-import "./App.scss";
-
-function App() {
-  return <div>Hello World!</div>;
-}
-
-export default App;
---
+++ client/src/components/App/App.js
@@ -0,0 +1,15 @@
+import Header from "../Header/Header";
+import "./App.scss";
+import Footer from "../Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>main</div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
