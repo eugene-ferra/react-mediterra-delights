5be0b2dcd58cee595b0a405c4bfd0ff7e3e98fcf
Eugene Kushnir <program.eugene@gmail.com>
2023-09-26 15:37:05 +0300

    crud operations refactoring

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -1,5 +1,5 @@
 import reviewModel from "../models/reviewModel.js";
-import catchAsync from "../utils/catchAsync.js";
+import * as factory from "./handleFactory.js";
 
 export const setReviewsIds = (req, res, next) => {
   if (!req.body.productID) req.body.productID = req.params.id;
@@ -8,95 +8,14 @@ export const setReviewsIds = (req, res, next) => {
   next();
 };
 
-export const addReview = catchAsync(async (req, res, next) => {
-  const doc = await reviewModel.create(req.body);
-  res.status(201).json({
-    status: "success",
-    data: doc,
-  });
+export const addReview = factory.addOne(reviewModel);
+export const getAllReviews = factory.getAll(reviewModel, {
+  path: "product",
+  select: "-reviews",
 });
-
-export const getAllReviews = catchAsync(async (req, res, next) => {
-  let queryObj = { ...req.query };
-
-  //deleting unnecesarry words from query string
-  const exludedValues = ["sort", "select", "page", "limit"];
-  exludedValues.forEach((el) => delete queryObj[el]);
-
-  //enable range filtering
-  let queryStr = JSON.stringify(queryObj);
-  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-  const query = reviewModel.find(JSON.parse(queryStr));
-
-  //sorting
-  if (req.query.sort) {
-    let sortStr = req.query.sort;
-    sortStr = sortStr.replace(",", " ");
-    query.sort(sortStr);
-  } else {
-    query.sort("title");
-  }
-
-  //selecting fields
-  if (req.query.select) {
-    let selectStr = req.query.select;
-    selectStr = selectStr.replace(",", " ");
-    query.select(selectStr);
-  }
-
-  if (req.query.page) {
-    const page = req.query.page;
-    const limit = req.query.limit || 3;
-    console.log(page, limit);
-
-    if (page < 1) return next(new AppError("Invalid page provided!", 404));
-    if (limit < 1) return next(new AppError("Invalid limiting!", 400));
-
-    query.skip((page - 1) * limit).limit(limit);
-  }
-
-  //executing query
-
-  const data = await reviewModel.find(query);
-
-  if (!data || data.length === 0)
-    return next(
-      new AppError("There aren't documents with current filters!", 404)
-    );
-
-  res.status(200).json({
-    status: "success",
-    data: data,
-  });
+export const getReview = factory.getOne(reviewModel, {
+  path: "product",
+  select: "-reviews",
 });
-
-export const getReview = catchAsync(async (req, res, next) => {
-  const doc = await reviewModel.findById(req.params.id);
-  if (!doc)
-    return next(new AppError("There aren't documents with this id!", 404));
-  res.status(201).json({
-    status: "success",
-    data: doc,
-  });
-});
-
-export const updateReview = catchAsync(async (req, res, next) => {
-  const doc = await reviewModel.findByIdAndUpdate(req.params.id, req.body);
-  if (!doc)
-    return next(new AppError("There aren't documents with this id!", 404));
-
-  res.status(200).json({
-    status: "success",
-    data: doc,
-  });
-});
-
-export const deleteReview = catchAsync(async (req, res, next) => {
-  const doc = await reviewModel.findByIdAndDelete(req.params.id);
-  if (!doc)
-    return next(new AppError("There aren't documents with this id!", 404));
-  res.status(204).json({
-    status: "success",
-    data: [],
-  });
-});
+export const updateReview = factory.updateOne(reviewModel);
+export const deleteReview = factory.deleteOne(reviewModel);
--- server/models/reviewModel.js
+++ server/models/reviewModel.js
@@ -1,34 +1,49 @@
 import mongoose from "mongoose";
 
-const reviewSchema = new mongoose.Schema({
-  productID: {
-    type: mongoose.Schema.ObjectId,
-    // required: [true, "Review must belong to product"],
-    // ref: "Product",
-  },
-  userID: {
-    type: mongoose.Schema.ObjectId,
-    required: [true, "Review must belong to user!"],
-    ref: "User",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  review: {
-    type: String,
-    trim: true,
-    maxLenght: [300, "Review must not contain more than 300 characters!"],
-  },
-  rating: {
-    type: Number,
-    enum: {
-      values: [1, 2, 3, 4, 5],
-      message:
-        "{VALUE} is not supported. Please provide rating equal to 1,2,3,4 or 5.",
+const reviewSchema = new mongoose.Schema(
+  {
+    productID: {
+      type: mongoose.Schema.ObjectId,
+      required: [true, "Review must belong to a product!"],
     },
-    required: [true, "Review must contain a rating!"],
+    userID: {
+      type: mongoose.Schema.ObjectId,
+      required: [true, "Review must belong to user!"],
+      ref: "User",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    review: {
+      type: String,
+      trim: true,
+      maxLenght: [300, "Review must not contain more than 300 characters!"],
+    },
+    rating: {
+      type: Number,
+      enum: {
+        values: [1, 2, 3, 4, 5],
+        message:
+          "{VALUE} is not supported. Please provide rating equal to 1,2,3,4 or 5.",
+      },
+      required: [true, "Review must contain a rating!"],
+    },
   },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
+
+reviewSchema.virtual("product", {
+  ref: "Product",
+  localField: "productID",
+  foreignField: "_id",
 });
 
 const reviewModel = mongoose.model("Review", reviewSchema);
